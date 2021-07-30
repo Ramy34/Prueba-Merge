9e19c1fbe1af49589e2dc8dd460a471b909e5fd1
ramy <ramses-c14@live.com.mx>
2021-07-30 15:10:45 -0500

    4

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,4 @@
 print("Hola Mundo escrito desde la rama Main")
 print("Hola, esto fue escrito en Desarrollo")
 print("Esto fue escrito en la rama de test")
+print("Continuo escribiendo en la rama Main")
